64a079c742f6b5fe77ad92b528d826656ad9bb58
Alex <alex5402private@gmail.com>
2024-02-23 20:45:19 +0530

    removed .cxx

--- Bcore/.cxx/Debug/6l6733g2/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/shadowhook/shadowhookConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET shadowhook::shadowhook)
-add_library(shadowhook::shadowhook SHARED IMPORTED)
-set_target_properties(shadowhook::shadowhook PROPERTIES
-    IMPORTED_LOCATION "/home/alex/.gradle/caches/transforms-3/b5cb4587a1f8b9dd87b5ac4af07cbb1a/transformed/jetified-shadowhook-1.0.8/prefab/modules/shadowhook/libs/android.armeabi-v7a/libshadowhook.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/alex/.gradle/caches/transforms-3/b5cb4587a1f8b9dd87b5ac4af07cbb1a/transformed/jetified-shadowhook-1.0.8/prefab/modules/shadowhook/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- Bcore/.cxx/RelWithDebInfo/5o6kt284/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/shadowhook/shadowhookConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET shadowhook::shadowhook)
-add_library(shadowhook::shadowhook SHARED IMPORTED)
-set_target_properties(shadowhook::shadowhook PROPERTIES
-    IMPORTED_LOCATION "/home/alex/.gradle/caches/transforms-3/e90b0947d631f38af080d445f1caeaae/transformed/shadowhook-1.0.8/prefab/modules/shadowhook/libs/android.arm64-v8a/libshadowhook.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/alex/.gradle/caches/transforms-3/e90b0947d631f38af080d445f1caeaae/transformed/shadowhook-1.0.8/prefab/modules/shadowhook/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
