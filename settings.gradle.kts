d57abd3ec10fe72627f93823ceeb00ed95324ddc
Alex <alex5402private@gmail.com>
2024-02-25 10:02:54 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        jcenter()
+    }
+}
+
+
+rootProject.name = "NewBlackbox-3.0"
+include(":app")
+include(":Bcore")
+
